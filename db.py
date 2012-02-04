a3508c9f1f38f838e328c907edfe3d719ea6fdd0
work computer <tinawenbj@gmail.com>
2012-02-04 16:36:30 -0500

    changed db.py again

--- db.py
+++ db.py
@@ -1,11 +1,13 @@
 from pymongo import Connection
 import os
+from urlparse import urlparse
 
 mongo_uri = os.getenv('MONGOLAB_URI')
 if mongo_uri is None:
     mongo_uri = 'mongodb://localhost:27017/test_database'
 
 connection = Connection(mongo_uri)
-db = connection.test_database
+db = connection[urlparse(mongo_uri).path[1:]]
 records = db.workouts
 users = db.users
+
