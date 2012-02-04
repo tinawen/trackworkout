0bae50ab50c8521655d949d15c39d53d4f705021
work computer <tinawenbj@gmail.com>
2012-02-04 16:28:54 -0500

    changed db configuration for deployment

--- db.py
+++ db.py
@@ -1,6 +1,11 @@
 from pymongo import Connection
+import os
 
-connection = Connection()
-db = connection['test_database']
+mongo_uri = os.getenv('MONGOLAB_URI')
+if mongo_uri is None:
+    mongo_uri = 'mongodb://localhost:27017/test_database'
+
+connection = Connection(mongo_uri)
+db = connection.test_database
 records = db.workouts
 users = db.users
